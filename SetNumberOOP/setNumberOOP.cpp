de63cc1b7462f4395bf2af4bdc1da1e99a6c6872
nad <154485919+nadinevaliaa@users.noreply.github.com>
2024-05-31 09:06:45 +0700

    Add files via upload

---
+++ SetNumberOOP/setNumberOOP.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Number1 {
+private:
+int y;
+
+public:
+    // setter
+    void setY(int a) {
+        y = a;
+    }
+    // getter
+    int getY() {
+        return y;
+    } 
+
+};
+
+class Number2 {
+public:
+   int x;
+};
+
+int main () {
+    Number1 satu;
+    Number2 dua;
+    
+    satu.setY(25);
+    dua.x = 30;
+
+    cout << "y :" << satu.getY() << endl;
+    cout << "x :" << dua.x << endl;
+
+    return 0;
+   
+}
